62d8582f6ca3a38341424b94aa75b76eb9e5cc4d
peteruithoven <puithoven@ecochain.com>
2018-06-07 09:20:32 +0200

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
@@ -65,6 +66,7 @@ var root = {
 };
 
 var app = express();
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
